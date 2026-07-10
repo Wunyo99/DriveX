0d998705945e4030937a684f0f4548b5f46596c2
Wunyo99 <mawunyoanani4@gmail.com>
2026-07-10 03:28:43 +0000

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,47 @@
+import { ArrowUpRight, Search } from "lucide-react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+const Hero = () => {
+  return (
+    <>
+      {/* <section className="h-screen flex items-cente justify-center md:justify-start overflow-hidden" > */}
+      <section className="h-screen flex items-cente md:block justify-center ">
+        <div
+          className="absolute inset-0 bg-cover lg:bg-center bg-no-repeat"
+          style={{
+            backgroundImage: `
+                          linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
+                    url('https://images.openai.com/static-rsc-4/M1Eo0yoaQv1LqfsbxAUAduOkmiNSBUj_jJcza2aSpHqpDWXD5dP5zbM2-422NbGhdPwZmhv5Sx5_11sl21hmV5frMAWv1fq9kUhIEk51n5fmIgZk-g_Gd_1QlHOeD1eR-hwYmi2J-h8eL0_ffVf_XvIi5Yt4VTATFq06r1GoUZw?purpose=inline')`,
+          }}
+        />
+
+        <div className="relative px-6 md:px-15 text-center md:text-start space-y-10 z-10">
+            <h1 className="text-center text-3xl md:text-7xl text-white font-bold tracking-wider mt-4">
+              <span className="text-green-900">DRIVE</span> YOUR <span className="text-lime-300">WAY</span>
+            </h1>
+
+          <div className="space-y-5 lg:mt-82 max-w-lg">
+            <p className="text-white/90 font-medium text-xl">
+              EXPLORE. BOOK. DRIVE
+            </p>
+
+            <div className="flex flex-col md:flex-row items-center justify-center md:justify-start gap-5">
+              <Link
+                to="/jobs"
+                className="flex items-center gap-2 text-black bg-lime-300 py-2 px-6 rounded-full font-medium hover:bg-yellow-200 duration-300"
+              >
+                <span>EXPLORE CARS</span>
+                <div className="bg-black p-1 rounded-full">
+                  {" "}
+                  <ArrowUpRight className="text-lime-400" />
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { Link, NavLink } from "react-router-dom";
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+const navLinks = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Cars",
+    path: "/cars",
+  },
+  {
+    name: "Services",
+    path: "/services",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-50 bg-green- backdrop-blur">
+      <div className="container mx-auto flex h-20 items-center justify-between px-4">
+        {/* Logo */}
+        <Link
+          to="/"
+          className="text-2xl md:text-4xl font-bold tracking-tight text-green-900"
+        >
+          Drive<span className="text-lime-300">X</span>
+        </Link>
+
+        {/* Desktop Navigation */}
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList className="gap-2">
+            {navLinks.map((link) => (
+              <NavigationMenuItem key={link.name}>
+                <NavigationMenuLink asChild>
+                  <NavLink
+                    to={link.path}
+                    className={({ isActive }) =>
+                      `rounded-md px-4 py-2 text-sm font-medium transition-colors ${
+                        isActive
+                          ? "bg-green-800 text-white"
+                          : "hover:bg-green-800 text-lime-300"
+                      }`
+                    }
+                  >
+                    {link.name}
+                  </NavLink>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        {/* Desktop Buttons */}
+        <div className="hidden items-center gap-3 lg:flex">
+          <Link className="bg-lime-300 py-1 px-4 rounded-md font-medium text-gray-800">
+            Login
+          </Link>
+
+          <Link className="bg-lime-300 py-1 px-4 rounded-md font-medium text-gray-800">
+            Sign Up
+          </Link>
+        </div>
+
+        {/* Mobile Menu */}
+        <div className="lg:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Menu className="h-6 w-6" />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent side="right" className="w-72">
+              <div className="mt-10 flex flex-col gap-2">
+                {navLinks.map((link) => (
+                  <NavLink
+                    key={link.name}
+                    to={link.path}
+                    className={({ isActive }) =>
+                      `rounded-md px-4 py-3 text-base font-medium transition ${
+                        isActive ? "bg-black text-white" : "hover:bg-gray-100"
+                      }`
+                    }
+                  >
+                    {link.name}
+                  </NavLink>
+                ))}
+
+                <div className="mt-6 flex flex-col gap-3">
+                  <Button variant="outline">Login</Button>
+
+                  <Button>Sign Up</Button>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Navbar;
