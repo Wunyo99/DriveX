0cbaaf5207d99a3d66a9233f849561a7581c0ab6
Wunyo99 <mawunyoanani4@gmail.com>
2026-07-31 02:39:46 +0000

    fix: Added scrollToTop functionality

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
 import "./index.css";
 import App from "./App.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop/>
       <App />
       <Toaster position="top-right" closeButton duration={5000} />
     </BrowserRouter>
